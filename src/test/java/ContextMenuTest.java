508023792a92fc5dc369b69416cddc3cd71f4757
ann <40922431+apanovich1@users.noreply.github.com>
2023-11-18 22:20:12 +0100

    file upload/frames tests

---
+++ src/test/java/ContextMenuTest.java
@@ -0,0 +1,49 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.util.concurrent.TimeUnit;
+
+public class ContextMenuTest {
+    //https://the-internet.herokuapp.com/context_menu
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(new String[]{"start-maximized"});
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void contextMenu() {
+        driver.get("https://the-internet.herokuapp.com/context_menu");
+        Actions actions = new Actions(driver);
+        actions.contextClick(driver.findElement(By.id("hot-spot")))
+                .perform();
+        Alert alert = driver.switchTo().alert();
+        String alertMessage = alert.getText();
+        Assert.assertEquals(alertMessage,"You selected a context menu",
+                "Incorrect alert text");
+        alert.dismiss();
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+
+        this.driver.quit();
+    }
+}
---
+++ src/test/java/FileUploadTest.java
@@ -0,0 +1,48 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import java.lang.String;
+import java.util.concurrent.TimeUnit;
+
+public class FileUploadTest {
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUp() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments(new String[]{"start-maximized"});
+        this.driver = new ChromeDriver(options);
+        this.driver.manage().timeouts().implicitlyWait(10L, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void fileUpload() {
+       /*File Upload
+        Загрузить файл
+        Проверить, что имя файла на странице совпадает с
+         именем загруженного файла*/
+        driver.get("https://the-internet.herokuapp.com/upload");
+
+        driver.findElement(By.id("file-upload")).sendKeys(
+                "C:\\Users\\annaa\\appHeroku\\src\\test\\resources\\fileToUpload.html");
+        driver.findElement(By.id("file-submit")).click();
+        String fileUploadedName = driver.findElement(By.id("uploaded-files")).getText();
+        Assert.assertEquals(fileUploadedName,"fileToUpload.html",
+                "Incorrect file was uploaded");
+    }
+
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+
+        this.driver.quit();
+    }
+}
